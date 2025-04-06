a1b0ad0cd3f9307c8ac9952831405188a95d9adc
Zachary A. Carmichael <zacac2024@outlook.com>
2025-04-06 16:50:10 -0600

    Started development of the CLI end of the application.

--- Main.py
+++ Main.py
@@ -14,6 +14,28 @@ from BBSearch import LstSearch
 if __name__ == "__main__":
     main()"""
 
+firstOpening = (
+"""
+            Hello! Welcome to ByteBungalow!
+            
+    Your one-stop shop for analyzing rental property data from some of the major rental sites and providing insightful graphs and filterable listings.
+"""
+)
+userName = ""
+searchCity = ""
+mainMenuText = (
+"""
+Welcome to ByteBungalow!
+    What would you like to do?
+        
+        1. Aggregate Listings (Update Database / Scour the Web for Listings!)
+        2. Edit your configurations
+        3. View your listings
+        4. Format Database
+        0. Exit
+
+"""
+)
 
 db = ListingDatabase()
 searchFunc = LstSearch()
@@ -22,25 +44,50 @@ searchFunc.setRent(0, 1500)
 searchFunc.setIsFavorited(True)
 searchFunc.getResults()
 
-#searchFunc.sortResults("rent", "asc")
+searchFunc.sortResults("rent", "desc")
 print(f"\n\n\n{'─'*198}\n      Sorted Listings:\n")
 print(f"   ID    {'Listing Name':<50}  {'Rent':<11} Rooms Utils    Host Site       Address                          URL                              Notes  \n{'─'*198}")
 for listing in searchFunc.temptList:
     print(listing)
     print(f"{'─'*198}")
 
-newListing = db.getListing(6)
-newListing.favorite()
-db.updateListing(newListing)
+#newListing = db.getListing(6)
+#newListing.favorite()
+#db.updateListing(newListing)
 
-"""while True:
-    print(
-    Welcome to ByteBungalow!
-    What would you like to do?
-        
-        1. Aggregate Listings (Update Database / Scour the Web for Listings!)
-        2. 
-)"""
+"""import os
+
+print(f"{'─'*198}")
+print(firstOpening)
+print(f"{'─'*198}")
+#userName = input("\tWhat would you like to be called?\n\t\tusername: ")
+print("\n\n\tWelcome to ByteBungalow, " + userName + "!")
+print(f"{'─'*198}")
+print("\n\n\tPlease give me one moment while I load your program...")
+os.system("cls")
+
+
+
+while choice != 0:
+    os.system("cls")
+    print(f"{'─'*198}")
+    print(mainMenuText)
+    print(f"{'─'*198}")
+    choice = int(input("\n\nInput:  "))
+
+    match choice:
+        case 1:
+            print("Aggregate Listings")
+        case 2:
+            print("Editing your configurations")
+        case 3:
+            print("Viewing your listings")
+        case 4:
+            print("Formatting Database")
+        case 0:
+            print("Goodbye!")
+            break"""
+
 
 db.close()
 
